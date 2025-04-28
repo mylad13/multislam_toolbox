38d36a8bc2b5c1c5f09caa9e2f8a1f491358d0b7
mylad13 <mfarjadnasab@gmail.com>
2025-04-28 08:50:21 -0400

    low_res slam added

--- launch/online_async_multirobot_launch.py
+++ launch/online_async_multirobot_launch.py
@@ -9,18 +9,12 @@ from ament_index_python.packages import get_package_share_directory
 from nav2_common.launch import RewrittenYaml
 from launch.conditions import IfCondition, UnlessCondition
 
-
 def generate_launch_description():
     pkg_dir = get_package_share_directory("slam_toolbox")
 
     use_sim_time = LaunchConfiguration('use_sim_time')
     namespace = LaunchConfiguration('namespace')
     resolution = LaunchConfiguration('resolution')
-
-    # remappings = [  ('/map', 'map'),
-    #                 ('/tf', 'tf'), 
-    #                 ('/tf_static', 'tf_static'),
-    #                 ('/map_metadata', 'map_metadata')]
 
     declare_use_sim_time_argument = DeclareLaunchArgument(
         'use_sim_time',
@@ -47,8 +41,11 @@ def generate_launch_description():
     # For high-resolution slam: publish on /map (i.e. no extra remapping needed)
     high_res_remappings = common_remappings + [('/map', 'map')]
     # For low-resolution slam: publish on /map_low instead
-    low_res_remappings = common_remappings + [('/map', 'map_low')]
-    
+    low_res_remappings = common_remappings + [
+        ('/map', 'map'), 
+        # ('/localized_scan', '/localized_scan_lowres')
+    ]
+
     # High resolution node (only runs if resolution != '1')
     high_res_slam = Node(
         package='slam_toolbox',
@@ -56,7 +53,7 @@ def generate_launch_description():
         name='slam_toolbox',
         namespace=namespace,
         output='screen',
-        remappings=high_res_remappings,  # Use default map topic for high-res
+        remappings=high_res_remappings,
         parameters=[
             ParameterFile(os.path.join(pkg_dir, 'config', 'mapper_params_online_multi_async.yaml'), allow_substs=True),
             {'use_sim_time': use_sim_time}
@@ -68,10 +65,10 @@ def generate_launch_description():
     low_res_slam = Node(
         package='slam_toolbox',
         executable='multirobot_slam_toolbox_node',
-        name='slam_toolbox_lowres',
+        name='slam_toolbox',
         namespace=namespace,
         output='screen',
-        remappings=low_res_remappings,  # Remap to map_low
+        remappings=low_res_remappings,
         parameters=[
             ParameterFile(os.path.join(pkg_dir, 'config', 'mapper_params_online_multi_async_lowres.yaml'), allow_substs=True),
             {'use_sim_time': use_sim_time}
